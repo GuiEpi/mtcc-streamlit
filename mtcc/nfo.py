fd14f4df7e5af3d32c67cb1fcaf096de9b03a6f1
GuiEpi <guillaume.coussot@epitech.eu>
2023-05-14 06:43:57 +0200

    :lipstick: makes nfo better

--- mtcc/nfo.py
+++ mtcc/nfo.py
@@ -1,12 +1,18 @@
+from datetime import datetime, timedelta
 from mako.template import Template
 from pymediainfo import MediaInfo
+import utils
 import json
 
 
 class Nfo:
     def __init__(self) -> None:
         self.tracks = []
-        self.properties = {"track_name_maxlen": 0}
+        self.properties = {
+            "track_name_maxlen": 0,
+            "playing_time": "",
+            "total_size": 0,
+        }
         self.template = Template(filename="./templates/nfo.mako")
         self.filename = "mtcc_no_name"
 
@@ -29,6 +35,7 @@ class Nfo:
 
     def parse(self, files) -> None:
         tag_files = self.get_tagfiles(files)
+        playing_time = []
 
         for tag_file in tag_files:
             track = {"track_name_len": 0}
@@ -45,6 +52,12 @@ class Nfo:
                 [self.properties["track_name_maxlen"], track["track_name_len"]]
             )
             self.properties["track_name_maxlen"] = longest_trackname
+            self.properties["total_size"] += track["file_size"]
+
+            # fix time error with the "duration" property
+            time_obj = datetime.strptime(track["other_duration"][4], "%H:%M:%S.%f")
+            track["duration"] = time_obj.strftime("%M:%S")
+            playing_time.append(track["duration"])
 
             self.tracks.append(track)
 
@@ -54,6 +67,22 @@ class Nfo:
             print("[ERROR] No tracks found")
 
         self.properties.update(self.tracks[0])
+        self.properties["total_size"] = utils.convert_size(
+            self.properties["total_size"]
+        )
+        self.properties["playing_time"] = self.__get_playing_time(playing_time)
         self.filename = (
             f"{self.properties['album']} [{self.properties['recorded_date']}]"
         )
+
+    def __get_playing_time(self, playing_times: dict[str]) -> str:
+        time_deltas = [
+            timedelta(minutes=int(time.split(":")[0]), seconds=int(time.split(":")[1]))
+            for time in playing_times
+        ]
+        total_time_delta = sum(time_deltas, timedelta())
+        total_seconds = int(total_time_delta.total_seconds())
+        minutes, seconds = divmod(total_seconds, 60)
+        total_time_formatted = f"{minutes:02}:{seconds:02}"
+
+        return total_time_formatted
